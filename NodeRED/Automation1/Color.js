65d3b1a94f13c9780687ed32e0ea90436b08687f
RouvenVogt <65019014+RouvenVogt@users.noreply.github.com>
2020-11-22 11:03:06 +0100

    Create Color.js

---
+++ NodeRED/Automation1/Color.js
@@ -0,0 +1,12 @@
+var payload = msg.payload.payload
+if (payload == "1"){
+    msg.payload = "0x0ee347"
+    }
+if (payload == "2"){
+    msg.payload = "0xe31c0e"
+    }
+if (payload == "3"){
+    msg.payload = "0x1c0ee3"
+    }
+
+return msg;
